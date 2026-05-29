d035f65248cb27e724cdbe87e4d29247165720b5
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-05-29 22:30:22 +0530

    Explored the assertArrayEquals() test method

--- src/test/java/com/telusko/CalculatorTest.java
+++ src/test/java/com/telusko/CalculatorTest.java
@@ -8,33 +8,46 @@ public class CalculatorTest {
 
     Calculator c = new Calculator();
 
-    @Test
-    void testAddition() {
+    // @Test
+    // void testAddition() {
 
-        int actual = c.add(2,3);
+    //     int actual = c.add(2,3);
 
-        assertEquals(5, actual);
-    }
+    //     assertEquals(5, actual);
+    // }
 
-    @Test
-    void testDivison(){
+    // @Test
+    // void testDivison(){
        
-        assertThrows(
-        ArithmeticException.class,
-        ()-> c.div(2, 0)
-        );
-    }
+    //     assertThrows(
+    //     ArithmeticException.class,
+    //     ()-> c.div(2, 0)
+    //     );
+    // }
 
-    @Test
-    void testmultiplication(){
-        int expected=c.mul(3, 2);
-        int actual=6;
-        assertEquals(expected, actual,"It didn't met the expectations");
-    }
+    // @Test
+    // void testmultiplication(){
+    //     int expected=c.mul(3, 2);
+    //     int actual=6;
+    //     assertEquals(expected, actual,"It didn't met the expectations");
+    // }
 
-    @Test
-    void testSubtraction(){
+    // @Test
+    // void testSubtraction(){
       
-        assertEquals(1 , c.sub(7, 6),  ()->"Test case failed");
+    //     assertEquals(1 , c.sub(7, 6),  ()->"Test case failed");  // effective way of passing a message, as message would only be considered when test case fails 
+    // }
+
+    @Test
+    void testAdd(){
+        assertEquals(5, c.add(2, 3)); // If expected == Actual value, it fails the test case, else pass. Reverse of assertEquals.
     }
-}
+
+    @Test
+
+    void testArray(){
+        int expArr[]={1,2,3,4};
+        int orgArr[]={3,4};
+        assertArrayEquals(expArr, orgArr, "Error reported");
+        }
+    }
